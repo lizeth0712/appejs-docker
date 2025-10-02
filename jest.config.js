72377cdeaf9d19a779c7b195b52e4b17c64296b2
lizeth <sotolizeth50@gmail.com>
2025-10-02 00:48:50 -0600

    test(ci): pruebas

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
-// jest.config.js
 module.exports = {
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.js"],
   collectCoverage: true,
   coverageReporters: ["text", "lcov", "cobertura"],
   coverageDirectory: "coverage",
-  moduleNameMapper: {
-    "^bcrypt$": "bcryptjs",
-    "\\.(css|png|jpg|jpeg)$": "<rootDir>/tests/__mocks__/fileMock.js"
-  }
+  moduleNameMapper: { "^bcrypt$": "bcryptjs" }
 };
